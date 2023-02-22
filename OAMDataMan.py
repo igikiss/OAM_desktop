83c5287881a974326febf740bbd63ed3b47c818b
igikiss <igor.kiss@icloud.com>
2023-02-22 16:36:01 +0100

    changes on coloring and chart functionality

--- OAMDataMan.py
+++ OAMDataMan.py
@@ -79,4 +79,5 @@ def rolling_mean(df, column, window, mena_value):
 roll_O2 = rolling_mean(df_filter_a, 'O2', '12H', df_f)
 roll_SpO2 = rolling_mean(df_filter_a, 'SpO2', '12H', df_filter_a['SpO2'].mean())
 roll_Pulse = rolling_mean(df_filter_a, 'PR', '12H', df_filter_a['PR'].mean())
+roll_PI = rolling_mean(df_filter_a, 'PI', '12H', df_filter_a['PI'].mean())
 
