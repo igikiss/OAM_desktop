f6727ec3916993afeac59efe200931ade2662f9c
igikiss <igor.kiss@icloud.com>
2024-02-18 09:52:02 +0100

    basic UI

--- New_layout.py
+++ New_layout.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QHBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QFrame, QFileDialog, QMessageBox
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon, QFont, QPixmap, QCursor, QImage, QColor
 import pyqtgraph as pg
@@ -40,13 +40,17 @@ class MainWindow(QMainWindow):
         layout.addStretch(1) 
 
         def button_func(name):
+            """This function creates a button with the given name and returns it.
+            We use these buttons  tp "Load Data" and "Snap Shot" in the GUI."""
             button = QPushButton(name)
             button.setFixedSize(200, 30)
-            # make button round 
+            button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
             button.setStyleSheet("border-radius: 10px; background-color: rgb(30, 195, 225); color: white;")
             return button
         
         def bar_graph_func():
+            """B ar_graph_func creates a bar graph and returns it. We use this to display the bar graph in the GUI.
+            We use these bar graphs to display the data of percetual use of FiO2 and Peercentual mode the device was used."""
             bar_graph = pg.PlotWidget()
             bar_graph.resize(300, 100)
             bar_graph.setBackground('transparent')
@@ -54,7 +58,15 @@ class MainWindow(QMainWindow):
             bar_graph.resize(300, 100)
             return bar_graph
         
+        def label_window(name):
+            """Label_window create labels whoch display the mean value of FiO2 and sesiion time"""
+            label = QLabel(name)
+            label.setFrameStyle(QFrame.Box | QFrame.Plain)
+            label.setLineWidth(1)
+            return label
+        
         def radio_button_func(name):
+    
             radio = QtWidgets.QRadioButton(name)
             return radio
         
@@ -69,9 +81,12 @@ class MainWindow(QMainWindow):
             return plot
         
         button1 = button_func('Load Data')
+        button1.clicked.connect(self.load_file)
+        
         button2 = button_func('Snap Shot')
         button2.setIcon(QIcon('snap.png'))
         button2.setIconSize(QtCore.QSize(30, 30))
+        button2.clicked.connect(self.take_screenshot)
         
         bar_graph = bar_graph_func()
         bar_graph2 = bar_graph_func()
@@ -90,6 +105,9 @@ class MainWindow(QMainWindow):
         table1.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         table1.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         table1.verticalHeader().setFont(font)
+        
+        label_window1 = label_window('Patient Information')
+        label_window2 = label_window('Ventilator Information')
         
         radio1 = radio_button_func('Option 1')
         radio2 = radio_button_func('Option 2')
@@ -112,28 +130,35 @@ class MainWindow(QMainWindow):
         layout3.addWidget(bar_graph2)
         layout3.addWidget(table1)
         
-        layout4 = QHBoxLayout()
-        layout4.addWidget(radio1)
-        layout4.addWidget(radio2)
-        layout4.addWidget(radio3)
+        layout4 = QVBoxLayout()
+        layout4.addWidget(label_window1)
+        layout4.addWidget(label_window2)
+    
+        layout3.addLayout(layout4)
         
         layout5 = QHBoxLayout()
-        layout5.addWidget(plot1)
+        layout5.addWidget(radio1)
+        layout5.addWidget(radio2)
+        layout5.addWidget(radio3)
         
         layout6 = QHBoxLayout()
-        layout6.addWidget(radio4)
-        layout6.addWidget(radio5)
-        layout6.addWidget(radio6)
+        layout6.addWidget(plot1)
         
-        layout7 = QHBoxLayout() 
-        layout7.addWidget(plot2)
+        layout7 = QHBoxLayout()
+        layout7.addWidget(radio4)
+        layout7.addWidget(radio5)
+        layout7.addWidget(radio6)
         
-        layout.addLayout(layout2)  # Add the QHBoxLayout to the QVBoxLayout
+        layout8 = QHBoxLayout() 
+        layout8.addWidget(plot2)
+        
+        layout.addLayout(layout2) 
         layout.addLayout(layout3)
         layout.addLayout(layout4)
         layout.addLayout(layout5)
         layout.addLayout(layout6)
         layout.addLayout(layout7)
+        layout.addLayout(layout8)
         
         central_widget = QWidget()
         central_widget.setLayout(layout)
@@ -141,6 +166,34 @@ class MainWindow(QMainWindow):
         
         self.show()
         
+    def load_file(self):
+        """Load a CSV file into a dataframe"""
+        file_path, _ = QFileDialog.getOpenFileName(self, "Open CSV", "", "CSV Files (*.csv)")
+        if file_path:
+            try:
+                df = pd.read_csv(file_path, encoding='unicode_escape', skiprows=1, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).dropna()
+                df['DateTime'] = pd.to_datetime(df['DateTime'])
+                df.set_index('DateTime', inplace=True)
+                df.columns = df.columns.str.replace(' ', '_')
+                self.df = df
+            except Exception as e:
+                QMessageBox.warning(self, "Error", f"Error loading CSV:\n{str(e)}")
+                
+    def take_screenshot(self):
+        """Take a screenshot of the current window and save it to a file"""
+        screen = QtGui.QGuiApplication.primaryScreen()
+        pixmap = screen.grabWindow(0)
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getSaveFileName(
+            self, "OAM Screenshot", "", "PNG Files (*.png)", options=options
+        )
+        if file_name:
+            pixmap.save(file_name, "PNG")
+            
+    
+        
+            
+            
 app = QApplication(sys.argv)
 if __name__ == '__main__':
     window = MainWindow()
